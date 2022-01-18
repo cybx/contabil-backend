b917ce73928675e48f167799bb497b716d1bc2fc
Victor Barbieri <victor@cybx.dev>
2022-01-18 02:44:39 -0300

    new files

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  roots: ['<rootDir>/src'],
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts', '!<rootDir>/src/main/**', '!<rootDir>/src/**/*-protocols.ts', '!**/protocols/**', '!**/test/**'],
+  // testEnvironment: 'node',
+  coverageDirectory: 'coverage',
+  coverageProvider: 'babel',
+  preset: '@shelf/jest-mongodb',
+  watchPathIgnorePatterns: ['globalConfig'],
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  }
+}
